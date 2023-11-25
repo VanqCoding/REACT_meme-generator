9c3b09fa6df12444f4e0311f84a057a63a228490
VanqCoding <96481194+VanqCoding@users.noreply.github.com>
2023-11-25 02:15:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# REACT_meme-generator
+ 
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src="https://pngimg.com/uploads/doge_meme/doge_meme_PNG7.png" 
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">Interactive React Project</h4>
+        </header>
+    )
+}
+
+
+
